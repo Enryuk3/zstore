fed809457305d808de1e938ffb69b91a568be9b4
Cristopher Guzmán <58184133+CristopherGuzman@users.noreply.github.com>
2022-04-24 22:10:41 -0500

    Update and rename Card.js to Item.js

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,15 +1,16 @@
-function Card() {
+function Item({item}) {
   return (
-    <div className="card w-96 bg-base-100 shadow-xl">
+    item.map(item => <div className="shadow-xl card w-96 bg-base-100" key={item.id} >
       <figure><img src="https://api.lorem.space/image/shoes?w=400&h=225" alt="Shoes" /></figure>
       <div className="card-body">
-        <h2 className="card-title">Shoes!</h2>
+        <h2 className="card-title">{item.title}</h2>
         <p>If a dog chews shoes whose shoes does he choose?</p>
-        <div className="card-actions justify-end">
+        <div className="justify-end card-actions">
           <button className="btn btn-primary">Buy Now</button>
         </div>
       </div>
     </div>
   )
-}
-export default Card
+  )}
+
+export default Item
