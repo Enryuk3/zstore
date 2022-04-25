25f7ab8bfee2ad2f25dad3f71c357a4ff3c962a8
Cristopher Guzmán <58184133+CristopherGuzman@users.noreply.github.com>
2022-04-24 22:15:28 -0500

    Update and rename Productos.js to ItemListContainer.js

---
+++ src/components/ItemListContainer.js
@@ -0,0 +1,10 @@
+import ItemList from "./ItemList"
+
+function ItemListContainer() {
+  return (
+    <div className="w-full">
+      <ItemList />
+    </div>
+  )
+}
+export default ItemListContainer
