05ca60b458148db1730d3187b2d0d680f850e587
CristopherGuzman <guzmncristopher@gmail.com>
2022-05-08 20:36:00 -0500

    ADD: useContext

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import './App.css';
-import Navbar from './components/Navbar';
-import ItemListContainer from './components/ItemListContainer';
-import Footer from './components/Footer';
-import { BrowserRouter, Route, Routes} from 'react-router-dom';
-import ItemDetailContainer from './components/ItemDetailContainer';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import ItemListContainer from "./components/ItemListContainer";
+import ItemDetailContainer from "./components/ItemDetailContainer";
+
+import CartContextProvider from "./components/context/CartContext";
+import AppContextProvider from "./components/context/AppContext";
 
 function App() {
   return (
-    <BrowserRouter>
-     <Navbar />
-     <Routes >
-       <Route path='/' element={ <ItemListContainer/> } />
-       <Route path='/shop/:categoryId' element={ <ItemListContainer/> } />
-       <Route path='/shoes/:shoesId' element={ <ItemDetailContainer/> } />    
-       <Route path='/all' element={ <ItemListContainer />} />
-     </Routes>
-     <Footer />
-    </BrowserRouter>
-
+    <AppContextProvider>
+      <CartContextProvider>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<ItemListContainer />} />
+            <Route path="/shop/:categoryId" element={<ItemListContainer />} />
+            <Route path="/shoes/:shoesId" element={<ItemDetailContainer />} />
+            <Route path="/all" element={<ItemListContainer />} />
+          </Routes>
+          <Footer />
+        </BrowserRouter>
+      </CartContextProvider>
+    </AppContextProvider>
   );
 }
 
