8989f0448fc223cbc9614702797d1801a3715002
CristopherGuzman <guzmncristopher@gmail.com>
2022-06-04 19:19:04 -0500

    ADD colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,17 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "z-pink": "#EC1075",
+        "z-blue": "#171524", //body
+        "z-dark-blue": "#070916", //header
+        "z-border": "#36364e",
+      },
+      fontFamily: {
+        oswald: ["Oswald", "sans-serif"],
+      },
+    },
   },
   plugins: [require("daisyui")],
   daisyui: {
